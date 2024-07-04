decd95f6536c4550284e02f5b52e186259ab5b91
twotwobread <dltndud9137@naver.com>
2024-07-05 08:38:30 +0900

    fix: modify code structure correctly

--- go/weather-forecast/weather_forecast.go
+++ go/weather-forecast/weather_forecast.go
@@ -1,8 +1,13 @@
+// Package weather is about weather.
 package weather
 
+// CurrentCondition represent weather condition now.
 var CurrentCondition string
+
+// CurrentLocation is where you are position.
 var CurrentLocation string
 
+// Forecast is function about printing weather condition about your location.
 func Forecast(city, condition string) string {
 	CurrentLocation, CurrentCondition = city, condition
 	return CurrentLocation + " - current weather condition: " + CurrentCondition
